efad226bf6c4ac368e8fee463dd26ad4b18f6776
Tushar Pai <tushara.cs19@bmsce.ac.in>
2021-06-09 19:37:52 +0530

    Added a feature that will store passwords in an encrypted form

--- api/routes/student_registration.js
+++ api/routes/student_registration.js
@@ -1,34 +1,44 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const con = require("../../database/db");
 const router = express.Router();
+const saltRounds = 10;
 
 
 
 router.get('/', function (req, res) {
-    res.render("student_registration.hbs");
-  });
+  res.render("student_registration.hbs");
+});
 
 
-  
+
 router.post('/', function (req, res) {
-    console.log(req.body);
-    if (req.body.password[0] == req.body.password[1]) {
-      let username = req.body.username;
-      let email = req.body.email;
-      let password = req.body.password[0];
+
+  if (req.body.password[0] == req.body.password[1]) {
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password[0];
+    console.log("Password is " + password);
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
       con.on('error', function (err) {
         console.log("[mysql error]", err);
       });
-  
-      con.query(`INSERT INTO student_registration(user_name, user_email, user_password) VALUES ('${username}', '${email}', '${password}');commit;`, function (err, result) {
+      con.query(`INSERT INTO student_registration(user_name, user_email, user_password) VALUES ('${username}', '${email}', '${hash}');commit;`, function (err, result) {
         if (err) throw err;
         console.log("values inserted");
+        res.render('student_registration', { msg1: 'successful', msg2: 'check();' });
       });
-  
-      res.render('student_registration', { msg1: 'successful', msg2: 'check();' });
-  
-    }
-  });
-  
-  module.exports = router;
+    });
+
+
+
+
+  }
+});
+
+module.exports = router;
+
+
+
 
